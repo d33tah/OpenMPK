168d897c25de30f2e9b7ad18ef0f24c5bc59ae19
pt12lol <pt12lol@gmail.com>
2012-10-30 03:26:34 -0700

    test wysyłania na gałąź

--- OpenMpk/src/main/java/pl/lodz/uni/math/Line.java
+++ OpenMpk/src/main/java/pl/lodz/uni/math/Line.java
@@ -2,6 +2,7 @@ package pl.lodz.uni.math;
 
 public class Line {
 	private String name;
+	//test Piotrka
 
 	public String getName() {
 		return name;
@@ -9,5 +10,5 @@ public class Line {
 
 	public void setName(String name) {
 		this.name = name;
-	}	
+	}
 }
